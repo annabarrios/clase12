868e414c2f674900c275e85ad03ba5bf45092df0
annabarrios <166767059+annabarrios@users.noreply.github.com>
2024-05-23 20:22:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var nombre=prompt("Cúal es su nombre?");
+
+if (nombre) {
+    function saludo(nombre) {
+    alert("Bienvenida");
+}
+saludo("nombre");
+} else {
+    alert("por favor ingrese su nombre");
+}
